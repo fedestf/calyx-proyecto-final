e56b8aee20a6231f283a5d3e8e60623da7da2448
Federico López <fede520@live.com>
2021-02-27 18:40:36 -0300

    mailer y scrapper
    falta acomodar el subject del mail y ya queda todo perfecto

--- Tarea 2/mailer.py
+++ Tarea 2/mailer.py
@@ -3,52 +3,66 @@ import ssl
 from settings import PASSWORD, MAIL
 from loggers import logger_debug, logger_error
 from email.message import EmailMessage
+import os
+import glob
+import scrapper
+
 
 context = ssl.create_default_context()
 
 
 lista_correo = ['fede520@live.com',
-                'moreyrajuanse@gmail.com',
-                'soria.lucas.e@gmail.com',
-                'moreyrajuanse@gmail.com',
-                'mariano.river@live.com.ar'
+                # 'moreyrajuanse@gmail.com',
+                # 'soria.lucas.e@gmail.com',
+                # 'moreyrajuanse@gmail.com',
+                # 'mariano.river@live.com.ar'
                 ]
+
+archivos = [r'Tarea 2\Noticias salida\economia.txt',
+            r'Tarea 2\Noticias salida\politica.txt',
+            r'Tarea 2\Noticias salida\principal.txt']
+
 
 destinatarios = ",".join(lista_correo)
 
 newMessage = EmailMessage()
-newMessage['Subject'] = "Lista de noticias de economia de la fecha"
+
 newMessage['From'] = MAIL
 newMessage['To'] = destinatarios
+newMessage['Subject'] = "Lista de noticias  de  la fecha"
 
-with open(r'Tarea 2\Noticias salida\economia.txt', 'r') as file:
-    file_data = file.read()
-    # file_name = "economia.pdf"
-
-newMessage.set_content(file_data)
-
-# newMessage.add_attachment(
-#     file_data, maintype='application', subtype='octet-stream', filename=file_name)
-
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+for archivo in archivos:
     try:
-        server.login(MAIL, PASSWORD)
-        print("se ha logueado con exito")
-        server.login(MAIL, PASSWORD)
-        server.send_message(newMessage)
-        logger_debug.debug(
-            f"se ha enviado correctamente el correo a {destinatarios}")
+        with open(archivo, 'r') as file:
+            file_data = file.read()
+            logger_debug.debug(f"Archivo {archivo} leido con exito")
     except Exception as e:
         logger_error.error(e)
 
-    # for correo in lista_correo:
-    #     try:
+    newMessage.set_content(file_data)
 
-    #         destinatario = correo
-    #         mensaje = ("Hola hijo de puta esto es un mail automatico")
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        try:
+            server.login(MAIL, PASSWORD)
+            print("se ha logueado con exito")
+            server.send_message(newMessage)
+            logger_debug.debug(
+                f"se ha enviado correctamente el correo a {destinatarios}")
+        except Exception as e:
+            logger_error.error(e)
 
-    #         server.sendmail(MAIL, destinatario, mensaje)
-    #         print(f"Mensaje enviado con exito a {destinatario}")
-    #         logger_debug.debug(f"Mensaje enviado con exito a {destinatario}")
-    #     except Exception as e:
-    #         logger_error.error(e)
+
+# BORRAR ARCHIVOS
+
+ruta = r'Tarea 2\Noticias salida\*.txt'
+lista_txt = glob.glob(ruta)
+
+
+for txt in lista_txt:
+    if txt.endswith('.txt'):
+        try:
+            os.remove(txt)
+            logger_debug.debug(f"El archivo {txt} ha sido eliminado")
+
+        except Exception as e:
+            logger_debug.error(e)
--- Tarea 2/scrapper.py
+++ Tarea 2/scrapper.py
@@ -2,8 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 from loggers import logger_debug, logger_error
 
-url_economia = 'https://www.infobae.com/economia/'
-page = requests.get(url_economia)
+# SCRAPPING PRINCIPAL
+url_principal = 'https://www.infobae.com/'
+page = requests.get(url_principal)
 soup = BeautifulSoup(page.content, 'html.parser')
 
 titular = soup.find_all('h2', class_="headline-title")
@@ -13,9 +14,36 @@ for elem in titular:
 
     sub = elem.find_all('a')
 
-    if sub[0].get('href').startswith('/economia'):  # SI HREF EMPIEZA CON /economia
+    with open(r'Tarea 2\Noticias salida\principal.txt', "a") as file:
 
-        with open(r'Tarea 2\Noticias salida\economia.txt', "a+") as file:
+        try:
+
+            file.write("\n"+sub[0].getText())  # textos de la etiqueta a
+            # concateno el link de la pagina al href
+            file.write("\n"+"https://www.infobae.com" +
+                       sub[0].get('href')+"\n")
+            logger_debug.debug(
+                f"Se añadio correctamente la noticia {sub[0].getText()}")
+
+        except Exception as e:
+            logger_error.error(e)
+
+# SCRAPPING POLITICA
+
+url_politica = 'https://www.infobae.com/politica/'
+page = requests.get(url_politica)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+titular = soup.find_all('h2', class_="headline-title")
+
+
+for elem in titular:
+
+    sub = elem.find_all('a')
+
+    if sub[0].get('href').startswith('/politica'):  # SI HREF EMPIEZA CON /economia
+
+        with open(r'Tarea 2\Noticias salida\politica.txt', "a") as file:
 
             try:
 
@@ -29,7 +57,31 @@ for elem in titular:
             except Exception as e:
                 logger_error.error(e)
 
+# SCRAPPING ECONOMIA
 
-# tags = soup('a')
-# for tag in tags:
-#     print(tag.get('href'))
+url_economia = 'https://www.infobae.com/economia/'
+page = requests.get(url_economia)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+titular = soup.find_all('h2', class_="headline-title")
+
+
+for elem in titular:
+
+    sub = elem.find_all('a')
+
+    if sub[0].get('href').startswith('/economia'):  # SI HREF EMPIEZA CON /economia
+
+        with open(r'Tarea 2\Noticias salida\economia.txt', "a") as file:
+
+            try:
+
+                file.write("\n"+sub[0].getText())  # textos de la etiqueta a
+                # concateno el link de la pagina al href
+                file.write("\n"+"https://www.infobae.com" +
+                           sub[0].get('href')+"\n")
+                logger_debug.debug(
+                    f"Se añadio correctamente la noticia {sub[0].getText()}")
+
+            except Exception as e:
+                logger_error.error(e)
